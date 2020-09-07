00bcc102d27ea1b11fead8e9fbe148697a79c465
GRANARocio <web2020.rocio@gmail.com>
2020-09-07 14:46:29 +0200

    solution exe 3

--- 03-switch.php
+++ 03-switch.php
@@ -1,29 +1,29 @@
 <?php
 if(isset($_GET['page'])){
     // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
-	
-	$p = $_GET['page'];
 
-	switch ($p) {
-		case 'Contactez-nous':
-			$content = '<h3>Contactez-nous</h3>';
-			break;
-		case 'Galerie':
-			$content = '<h3>Galerie</h3>';
-			break;
-		case 'Photos':
-			$content = '<h3>Photos</h3>';
-			break;
-		case 'Plan':
-			$content = '<h3>Plan</h3>';
-			break;
-		case 'Info':
-			$content = '<h3>Info</h3>';
-			break;
-		default:
-			$content = '<h3>Erreur 404</h3>';
-	}
-	
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
 }else{
     $content = "<h3>Ceci est la page d'accueil</h3>";
 }
@@ -45,6 +45,630 @@ if(isset($_GET['page'])){
 <a href="?page=photos">photos</a>
 <a href="?page=plan">plan</a>
 <a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'contactez-nous':
+            $content = '<h3>contactez-nous</h3>';
+            break;
+        case 'galerie':
+            $content = '<h3>galerie</h3>';
+            break;
+        case 'photos':
+            $content = '<h3>photos</h3>';
+            break;
+        case 'plan':
+            $content = '<h3>plan</h3>';
+            break;
+        case 'info':
+            $content = '<h3>info</h3>';
+            break;
+        default:
+            $content = '<h3>Erreur 404</h3>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">contactez-nous</a>
+<a href="?page=galerie">galerie</a>
+<a href="?page=photos">photos</a>
+<a href="?page=plan">plan</a>
+<a href="?page=info">info</a>
+<?php
+echo $content;
+?>
+</body>
+</html>
+<?php
+if(isset($_GET['page'])){
+    // avec un switch, changez la variable $content suivant le lien cliqué, si la valeur de la variable get "page" est différente de celles des liens, affichage du texte: Erreur 404
+
+    $p = $_GET['page'];
+
+    switch ($p) {
+        case 'Contactez-nous':
+            $content = '<h4>contactez-nous</h4>';
+            break;
+        case 'Galerie':
+            $content = '<h4>galerie</h4>';
+            break;
+        case 'Photos':
+            $content = '<h4>photos</h4>';
+            break;
+        case 'Plan':
+            $content = '<h4>plan</h4>';
+            break;
+        case 'Info':
+            $content = '<h4>info</h4>';
+            break;
+        default:
+            $content = '<h4>Erreur 404</h4>';
+    }
+
+}else{
+    $content = "<h3>Ceci est la page d'accueil</h3>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Switch</title>
+</head>
+<body>
+<h1>Switch</h1>
+<a href="03-switch.php">Accueil</a>
+<a href="?page=contactez-nous">Contactez-nous</a>
+<a href="?page=galerie">Galerie</a>
+<a href="?page=photos">Photos</a>
+<a href="?page=plan">Plan</a>
+<a href="?page=info">Info</a>
 <?php
 echo $content;
 ?>
